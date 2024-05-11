23ecf1cbd2df0af81a9b9337cb3ce9380677d1e3
Alfie728 <zegro728@gmail.com>
2024-05-11 18:00:49 +0800

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ShinyButton from "./ShinyButton";
+
+function App() {
+  return (
+    <main className="h-full w-full grid place-content-center">
+      <ShinyButton />
+    </main>
+  );
+}
+
+export default App;
